c8a5bf27623b75e9445cb511c97ba63c16729334
Irfan Baig <irfan@grati.io>
2018-03-25 14:13:02 -0700

    upgrade to 5.2 -- not backwards cmpatible for now

--- app/helpers/lockup/lockup_helper.rb
+++ app/helpers/lockup/lockup_helper.rb
@@ -2,7 +2,7 @@ module Lockup
   module LockupHelper
 
     def lockup_hint_present?
-      if ENV["LOCKUP_HINT"].present? || ENV["lockup_hint"].present? || ((Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1) && Rails.application.secrets.lockup_hint.present?)
+      if ENV["LOCKUP_HINT"].present? || ENV["lockup_hint"].present? || ((Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 2) && Rails.application.credentials[:lockup_hint].present?)
         true
       else
         false
@@ -14,8 +14,8 @@ module Lockup
         ENV["LOCKUP_HINT"].to_s
       elsif ENV["lockup_hint"].present?
         ENV["lockup_hint"].to_s
-      elsif (Rails::VERSION::MAJOR >= 4 && Rails::VERSION::MINOR >= 1) && Rails.application.secrets.lockup_hint.present?
-        Rails.application.secrets.lockup_hint.to_s
+      elsif (Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 2) && Rails.application.credentials[:lockup_hint].present?
+        Rails.application.credentials.lockup_hint.to_s
       end
     end
 
